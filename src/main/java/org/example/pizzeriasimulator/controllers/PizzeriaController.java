7da856158ee6e152a2723fd8c4bbf6b3ec080389
pavlo-kokhan <pavlokokhan@gmail.com>
2024-12-03 23:13:48 +0200

    Cors

--- src/main/java/org/example/pizzeriasimulator/controllers/PizzeriaController.java
+++ src/main/java/org/example/pizzeriasimulator/controllers/PizzeriaController.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/pizzeria")
+@CrossOrigin(origins = "http://localhost:3000")
 public class PizzeriaController {
     private PizzeriaSimulationService service;
 
